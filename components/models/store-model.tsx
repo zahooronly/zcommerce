b2d4304cac02e4b66b72d62d44fd3427b26d51d3
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-07-18 15:24:27 +0500

    store

--- components/models/store-model.tsx
+++ components/models/store-model.tsx
@@ -2,20 +2,40 @@
 
 import { useStoreModal } from "@/hooks/use-store-modal";
 import { Modal } from "@/components/ui/modal";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import * as z from "zod";
 // import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
-export const StoreSchema = z.object({
+export const formSchema = z.object({
   name: z.string().min(3).max(255),
 });
 
 export const StoreModel = () => {
   const storeModel = useStoreModal();
-  const form = useForm<z.infer<typeof StoreSchema>>({
-    resolver: zodResolver(StoreSchema),
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
   });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+    // Create Form
+  };
+
   return (
     <Modal
       title="Create Store"
@@ -23,18 +43,33 @@ export const StoreModel = () => {
       isOpen={storeModel.isOpen}
       onClose={storeModel.onClose}
     >
-      Future Create Store Form
+      <div>
+        <div className="space-y-4 py-2 pb-4">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="E-Commerce" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                <Button variant="outline" onClick={storeModel.onClose}>
+                  Cancel
+                </Button>
+                <Button type="submit">Continue</Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </div>
     </Modal>
   );
 };
-function zodResolver(
-  StoreSchema: z.ZodObject<
-    { name: z.ZodString },
-    "strip",
-    z.ZodTypeAny,
-    { name: string },
-    { name: string }
-  >
-): import("react-hook-form").Resolver<{ name: string }, any> | undefined {
-  throw new Error("Function not implemented.");
-}
